4068361b289afea80111d485eeaaf2a512c836f3
yukmakoto <patchcat620@gmail.com>
2026-01-02 15:33:17 +0800

    feat: Configure for yukmakoto fork with gray tip detection

--- get_artifacts.sh
+++ get_artifacts.sh
@@ -2,13 +2,14 @@
 
 token="$1"
 version="$2"
+repo="${3:-NapNeko/NapCatQQ}"
 
 # 设置输出目录
 output_dir="."
 # 下载release
 curl -s -X GET \
     -H "Authorization: token $token" \
-    -L "https://github.com/NapNeko/NapCatQQ/releases/download/$version/NapCat.Shell.zip" \
+    -L "https://github.com/$repo/releases/download/$version/NapCat.Shell.zip" \
     -o "$output_dir/NapCat.Shell.zip"
 
 echo "编译产物已保存到$output_dir"
